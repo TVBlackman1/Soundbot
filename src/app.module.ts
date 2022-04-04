fd58a43d0b46d008cd0b7a4839a3a53bc7b67874
tvblackman1 <vbif.misha2012@yandex.ru>
2022-04-05 02:23:35 +0300

    created module of projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import {User} from './users/users.model';
 import {ConfigModule} from './config/config.module';
 import {MusicTrackModule} from './music-track/music-track.module';
 import {GettersConfigService} from './config/config.service';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -28,6 +29,7 @@ import {GettersConfigService} from './config/config.service';
     UsersModule,
     ConfigModule,
     MusicTrackModule,
+    ProjectsModule,
   ],
   controllers: [AppController,],
   providers: [AppService,],
